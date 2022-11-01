68e21e28587db687de0be7f776e0b3808d36cb13
sumeghspai <sumeghspai@gmail.com>
2022-11-01 16:35:33 +0530

    codeofconduct done

--- src/components/CodeofConduct/CodeofConduct.js
+++ src/components/CodeofConduct/CodeofConduct.js
@@ -1,9 +1,26 @@
 import styles from './CodeofConduct.module.css'
+import down from '../../assets/svg/download.svg'
 function CodeOfConduct()
 {
     return(
         <>
-        <div className={styles.codeofconduct}>Code Of Conduct</div>
+        <div className={styles.codeofconduct}>
+        <div className={styles.heading}> CODE OF CONDUCT</div>
+            <div className={styles.code}>
+               <div>Download Code of Conduct for MEC.conf</div>
+               <div className='img'>
+               <img src={down}/> 
+               </div>
+                
+
+            </div>
+        
+        
+        
+        
+        
+        
+        </div>
         </>
     );
 }
--- src/components/Sponsors/Sponsors.js
+++ src/components/Sponsors/Sponsors.js
@@ -1,9 +1,73 @@
 import styles from './Sponsors.module.css'
+import sponsorImg from '../../assets/sponsors/tinker.jpg'
 function Sponsors()
 {
     return(
         <>
-        <div className={styles.sponsors}>Sponsors</div>
+        <div className={styles.sponsors}>
+            <h1>Sponsors</h1>
+            <div className={styles.cards}>
+            
+                <div class={styles.img}>
+                    <img src={sponsorImg}></img>
+                </div>
+                <div class={styles.caption}>
+                    <h3>Company 1</h3>
+                    
+                    
+                </div>
+
+
+          
+
+            </div>      
+            <div className={styles.cards}>
+            
+            <div class={styles.img}>
+                <img src={sponsorImg}></img>
+            </div>
+            <div class={styles.caption}>
+                <h3>Company 1</h3>
+                
+                
+            </div>
+
+
+      
+
+        </div>  
+        <div className={styles.cards}>
+            
+            <div class={styles.img}>
+                <img src={sponsorImg}></img>
+            </div>
+            <div class={styles.caption}>
+                <h3>Company 1</h3>
+                
+                
+            </div>
+
+
+      
+
+        </div>  
+        <div className={styles.cards}>
+            
+            <div class={styles.img}>
+                <img src={sponsorImg}/>
+            </div>
+            <div class={styles.caption}>
+                <h3>Company 1</h3>
+                
+                
+            </div>
+
+
+      
+
+        </div>   
+        
+        </div>
         </>
     );
 }
