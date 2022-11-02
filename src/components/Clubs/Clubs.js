f103817b73a909c7838033fcb2a99d8ea4a07384
subru-37 <easwaransubramaniOO7@gmail.com>
2022-11-02 10:09:44 +0530

    fixed design not loading in background (circles)

--- src/components/Clubs/Clubs.js
+++ src/components/Clubs/Clubs.js
@@ -2,11 +2,12 @@ import styles from './Clubs.module.css';
 import react from 'react';
 import Cards from './Cards';
 import {data} from './CardData';
+import bg from '../../assets/svg/Group_9.svg';
 export default function Clubs()
 {
     return(
         <div className={styles.clubs}>
-            <div className={styles.styling}></div>
+            <div className={styles.styling} style={{backgroundImage:`url(${bg.src})`}}></div>
             <p className={styles.Par}>Clubs</p>
             <div className={styles.clubCards}>
                 {
