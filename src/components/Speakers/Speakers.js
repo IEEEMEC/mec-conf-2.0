074555cfb073cbca24056b88b78c20059a0583d1
jaison080 <jaisondennis080@gmail.com>
2022-11-15 18:55:55 +0530

    Speakers Completed

--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -1,10 +1,31 @@
-import styles from './Speakers.module.css'
-function Speakers()
-{
-    return(
-        <>
-        <div className={styles.speakers}>Speakers</div>
-        </>
-    );
+import Image from "next/image";
+import speakerData from "../../data/speakerData";
+import styles from "./Speakers.module.css";
+function Speakers() {
+  return (
+    <>
+      <div className={styles.speakers_container}>
+        <div className={styles.speakers}>
+          EVENT <span className={styles.speakers_1}>SPEAKERS</span>
+        </div>
+        <div className={styles.speakers_content}>
+          <div className={styles.speakers_left}></div>
+          <div className={styles.speakers_list}>
+            {speakerData.map((speaker) => {
+              return (
+                <div key={speaker.id} className={styles.speaker}>
+                  <div className={styles.speaker_image}>
+                    <Image src={speaker.image} alt={speaker.name} className={styles.speaker_image} />
+                  </div>
+                  <div className={styles.speaker_name}>{speaker.name}</div>
+                </div>
+              );
+            })}
+          </div>
+          <div className={styles.speakers_right}></div>
+        </div>
+      </div>
+    </>
+  );
 }
-export default Speakers
+export default Speakers;
---
+++ src/data/speakerData.js
@@ -0,0 +1,44 @@
+import user from "../assets/speakers/user.jpg";
+const speakerData = [
+  {
+    id: 1,
+    name: "Bucky Roberts",
+    image: user,
+  },
+  {
+    id: 2,
+    name: "Joby Williams",
+    image: user,
+  },
+  {
+    id: 3,
+    name: "Madison Price",
+    image: user,
+  },
+  {
+    id: 4,
+    name: "Oliver Karlsson",
+    image: user,
+  },
+  {
+    id: 5,
+    name: "Noah Oliver",
+    image: user,
+  },
+  {
+    id: 6,
+    name: "Emma Smith",
+    image: user,
+  },
+  {
+    id: 7,
+    name: "Jack Anderson",
+    image: user,
+  },
+  {
+    id: 8,
+    name: "Ava Taylor",
+    image: user,
+  },
+];
+export default speakerData;
