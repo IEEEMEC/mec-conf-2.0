af52e4901046439056b9d0e494986ec088f5c301
jaison080 <jaisondennis080@gmail.com>
2022-11-15 16:56:56 +0530

    Workshops Complete

---
+++ src/components/Workshops/WorkshopCard.js
@@ -0,0 +1,10 @@
+import styles from './Workshops.module.css'
+function WorkshopCard()
+{
+    return(
+        <div className={styles.card}>
+
+        </div>
+    );
+}
+export default WorkshopCard
--- src/components/Workshops/Workshops.js
+++ src/components/Workshops/Workshops.js
@@ -1,10 +1,34 @@
-import styles from './Workshops.module.css'
-function Workshops()
-{
-    return(
-        <>
-        <div className={styles.workshops}>Workshops</div>
-        </>
-    );
+import Image from "next/image";
+import WorkshopCard from "./WorkshopCard";
+import styles from "./Workshops.module.css";
+import lefttop from "../../assets/workshops/lefttop.svg";
+import leftbottom from "../../assets/workshops/leftbottom.svg";
+import righttop from "../../assets/workshops/righttop.svg";
+import rightcenter from "../../assets/workshops/rightcenter.svg";
+import rightbottom from "../../assets/workshops/rightbottom.svg";
+function Workshops() {
+  return (
+    <>
+      <div className={styles.workshops_container}>
+        
+        <div className={styles.workshops}>
+          WORKSHOPS & <span className={styles.workshops_1}>TUTORIALS</span>
+        </div>
+        <div className={styles.cards}>
+          <WorkshopCard />
+          <WorkshopCard />
+          <WorkshopCard />
+          <WorkshopCard />
+          <WorkshopCard />
+          <WorkshopCard />
+        </div>
+        <Image src={lefttop} alt="" className={styles.lefttop} />
+        <Image src={leftbottom} alt="" className={styles.leftbottom} />
+        <Image src={righttop} alt="" className={styles.righttop} />
+        <Image src={rightcenter} alt="" className={styles.rightcenter} />
+        <Image src={rightbottom} alt="" className={styles.rightbottom} />
+      </div>
+    </>
+  );
 }
-export default Workshops
+export default Workshops;
