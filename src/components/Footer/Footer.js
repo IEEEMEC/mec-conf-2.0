646b70cd2d17f0616471ffe2488ddb0aecb75036
jaison080 <jaisondennis080@gmail.com>
2022-11-15 18:09:11 +0530

    Footer Complete

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,21 @@
-import styles from './Footer.module.css'
-function Footer()
-{
-    return(
-        <>
-        <div className={styles.footer}>Footer</div>
-        </>
-    );
+import Image from "next/image";
+import styles from "./Footer.module.css";
+import logo from '../../assets/footer/logo.png';
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa';
+import {AiFillTwitterCircle} from 'react-icons/ai';
+function Footer() {
+  return (
+    <>
+      <div className={styles.footer_container}>
+        <div className={styles.social_icons}> 
+            <FaFacebook size={40} color="white"/>
+            <FaInstagram size={40} color="white"/>
+            <FaLinkedin size={40} color="white"/>
+            <AiFillTwitterCircle size={40} color="white"/>
+        </div>
+        <Image src={logo} className={styles.logo} alt="" />
+      </div>
+    </>
+  );
 }
-export default Footer
+export default Footer;
